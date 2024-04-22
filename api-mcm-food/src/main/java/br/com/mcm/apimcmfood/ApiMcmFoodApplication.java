37991afd245ab63edb0da3c1c083fc09862ae206
MarcosCesarMorais <morais@CURSISPC5249.interno.livrariascuritiba.com.br>
2024-04-22 09:56:16 -0300

    Criacao do projeto

---
+++ api-mcm-food/src/main/java/br/com/mcm/apimcmfood/ApiMcmFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.mcm.apimcmfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMcmFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMcmFoodApplication.class, args);
+	}
+
+}
